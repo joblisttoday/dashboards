cbb855a1d24b386cfb7a289b87626de75ed54a49
ug.rp <ugrp@duck.com>
2024-04-08 16:40:43 +0200

    fix url profiles company

--- docs/components/table-jobs.js
+++ docs/components/table-jobs.js
@@ -5,7 +5,7 @@ export default function tableJobs(data = [], size = {}) {
 	const $table = Inputs.table(data, {
 		format: {
 			company_id: (x) =>
-				html`<a href="https://profiles.joblist.today/companies/${x}">${x}</a>`,
+				html`<a href="https://joblist.today/${x}">${x}</a>`,
 			url: (x) => html`<a href="${x}">${x}</a>`,
 		},
 	});
